fbddf739177fa5f1c9c4889b01fab8dc02a5bee0
Wayne <h88377@gmail.com>
2023-05-31 16:57:34 +0800

    Create inital project

---
+++ JKOAssignment/JKOAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JKOAssignment
+//
+//  Created by 鄭昭韋 on 2023/5/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
