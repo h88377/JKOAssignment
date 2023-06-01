562b11c694d483b984d56e58af86372aeba70d1a
Wayne <h88377@gmail.com>
2023-06-01 17:50:29 +0800

    Load CoreData persistent store from `NSPersistentContainer` with explicit `bundle` and `storeURL`

--- JKOAssignment/JKOAssignment/ItemList/Local/CoreDataStore.swift
+++ JKOAssignment/JKOAssignment/ItemList/Local/CoreDataStore.swift
@@ -5,10 +5,44 @@
 //  Created by 鄭昭韋 on 2023/6/1.
 //
 
-import Foundation
+import CoreData
 
 final class CoreDataStore: ItemStoreSaver {
+    private let container: NSPersistentContainer
+    private let context: NSManagedObjectContext
+    
+    init(bundle: Bundle = .main, storeURL: URL) throws {
+        container = try NSPersistentContainer.load(modelName: "PetStore", in: bundle, storeURL: storeURL)
+        context = container.newBackgroundContext()
+    }
+    
     func insert(_ item: Item, completion: @escaping (ItemStoreSaver.Result) -> Void) {
+       
+    }
+}
+
+extension NSPersistentContainer {
+    enum LoadingError: Error {
+        case modelNotFound
+        case failedToLoadPersistentStore(Error)
+    }
+    
+    static func load(modelName: String, in bundle: Bundle, storeURL: URL) throws -> NSPersistentContainer {
+        guard let modelURL = bundle.url(forResource: modelName, withExtension: "momd"), let model = NSManagedObjectModel(contentsOf: modelURL) else {
+            throw LoadingError.modelNotFound
+        }
         
+        let container = NSPersistentContainer(name: modelName, managedObjectModel: model)
+        let description = NSPersistentStoreDescription(url: storeURL)
+        container.persistentStoreDescriptions = [description]
+        
+        var loadError: Error?
+        container.loadPersistentStores { _, error in
+            loadError = error
+        }
+        
+        try loadError.map { throw LoadingError.failedToLoadPersistentStore($0) }
+        
+        return container
     }
 }
