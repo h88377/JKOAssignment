4d41c341d3d21ece8939b70047688ade1f1908b0
Wayne <h88377@gmail.com>
2023-06-01 15:20:19 +0800

    Configure `ItemDetailCell` layout

--- JKOAssignment/JKOAssignment/ItemList/UI/View/ItemDetailCell.swift
+++ JKOAssignment/JKOAssignment/ItemList/UI/View/ItemDetailCell.swift
@@ -31,4 +31,37 @@ final class ItemDetailCell: UITableViewCell {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
+    
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setUpUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpUI() {
+        contentView.addSubviews([nameLabel, descriptionLabel, priceLabel])
+        
+        let descriptionBottomConstraint = descriptionLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8)
+        descriptionBottomConstraint.priority = .defaultHigh
+        
+        NSLayoutConstraint.activate([
+            nameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            nameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            nameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+            
+            priceLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
+            priceLabel.trailingAnchor.constraint(equalTo: nameLabel.trailingAnchor),
+            priceLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            
+            descriptionLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
+            descriptionLabel.trailingAnchor.constraint(equalTo: nameLabel.trailingAnchor),
+            descriptionLabel.topAnchor.constraint(equalTo: priceLabel.bottomAnchor, constant: 8),
+            descriptionBottomConstraint
+        ])
+    }
 }
