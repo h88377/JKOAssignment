ca376f8e737ca1f407e001aa6f0f71e2f6d7d899
Wayne <h88377@gmail.com>
2023-06-01 17:03:13 +0800

    Create `LocalCartSaver` file

---
+++ JKOAssignment/JKOAssignment/ItemList/Local/LocalCartSaver.swift
@@ -0,0 +1,15 @@
+//
+//  LocalCartSaver.swift
+//  JKOAssignment
+//
+//  Created by 鄭昭韋 on 2023/6/1.
+//
+
+import Foundation
+
+final class LocalCartSaver: CartSaver {
+    
+    func save(item: Item, completion: @escaping (CartSaver.Result) -> Void) {
+        
+    }
+}
