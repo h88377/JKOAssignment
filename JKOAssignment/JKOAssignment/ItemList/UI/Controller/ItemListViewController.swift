0cb9dc4166c4a58a34e3888f9ce58d6beba2eb6b
Wayne <h88377@gmail.com>
2023-06-01 10:35:38 +0800

    Configure collection view UI of `ItemListViewController`

--- JKOAssignment/JKOAssignment/ItemList/UI/Controller/ItemListViewController.swift
+++ JKOAssignment/JKOAssignment/ItemList/UI/Controller/ItemListViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 final class ItemListViewController: UICollectionViewController {
+    private let viewModel: ItemListViewModel
+        
+    init(viewModel: ItemListViewModel) {
+        self.viewModel = viewModel
+        super.init(collectionViewLayout: .init())
+    }
+    
     private lazy var dataSource: UICollectionViewDiffableDataSource<Int, ItemListCellViewModel> = {
         .init(collectionView: collectionView) { collectionView, indexPath, viewModel in
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ItemListCell.identifier, for: indexPath) as? ItemListCell else { return UICollectionViewCell() }
@@ -22,13 +29,6 @@ final class ItemListViewController: UICollectionViewController {
     
     private var itemsSection: Int { return 0 }
     
-    private let viewModel: ItemListViewModel
-        
-    init(viewModel: ItemListViewModel) {
-        self.viewModel = viewModel
-        super.init(collectionViewLayout: .init())
-    }
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -36,7 +36,7 @@ final class ItemListViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView.refreshControl = binded(refreshView: UIRefreshControl())
+        configureCollectionView()
         loadItems()
     }
     
@@ -45,6 +45,29 @@ final class ItemListViewController: UICollectionViewController {
         snapshot.appendSections([itemsSection])
         snapshot.appendItems(newItems, toSection: itemsSection)
         dataSource.apply(snapshot, animatingDifferences: false)
+    }
+    
+    private func configureCollectionView() {
+        collectionView.backgroundColor = .systemGray6
+        collectionView.collectionViewLayout = configureCollectionViewLayout()
+        collectionView.dataSource = self.dataSource
+        collectionView.register(ItemListCell.self, forCellWithReuseIdentifier: ItemListCell.identifier)
+        collectionView.refreshControl = binded(refreshView: UIRefreshControl())
+    }
+    
+    private func configureCollectionViewLayout() -> UICollectionViewCompositionalLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .absolute(290))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(290))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 2)
+        group.interItemSpacing = .fixed(8)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8)
+        section.interGroupSpacing = 8
+        
+        return UICollectionViewCompositionalLayout(section: section)
     }
     
     private func binded(refreshView: UIRefreshControl) -> UIRefreshControl {
