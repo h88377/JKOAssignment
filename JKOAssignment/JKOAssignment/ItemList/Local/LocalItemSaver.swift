b4a58000a1d6173bf829ec4ef0daca76a5af2be6
Wayne <h88377@gmail.com>
2023-06-01 17:17:54 +0800

    Hide the local saving framework detail with a protocol `ItemStoreSaver`

--- JKOAssignment/JKOAssignment/ItemList/Local/LocalItemSaver.swift
+++ JKOAssignment/JKOAssignment/ItemList/Local/LocalItemSaver.swift
@@ -7,9 +7,29 @@
 
 import Foundation
 
+protocol ItemStoreSaver {
+    typealias Result = Error?
+    
+    func insert(_ item: Item, completion: @escaping (Result) -> Void)
+}
+
 final class LocalItemSaver: ItemSaver {
+    enum SaveError: Error {
+        case failed
+    }
+    
+    private let storeSaver: ItemStoreSaver
+    
+    init(storeSaver: ItemStoreSaver) {
+        self.storeSaver = storeSaver
+    }
     
     func save(item: Item, completion: @escaping (ItemSaver.Result) -> Void) {
-        
+        storeSaver.insert(item) { error in
+            guard error == nil else {
+                return completion(.some(SaveError.failed))
+            }
+            completion(.none)
+        }
     }
 }
