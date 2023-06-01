41fa31fdba8a726e2d5d86cbd278eaaaa72e6150
Wayne <h88377@gmail.com>
2023-06-01 17:03:51 +0800

    Rename `LocalCartSaver`

--- JKOAssignment/JKOAssignment/ItemList/Local/LocalItemSaver.swift
+++ JKOAssignment/JKOAssignment/ItemList/Local/LocalItemSaver.swift
@@ -1,5 +1,5 @@
 //
-//  LocalCartSaver.swift
+//  LocalItemSaver.swift
 //  JKOAssignment
 //
 //  Created by 鄭昭韋 on 2023/6/1.
@@ -7,7 +7,7 @@
 
 import Foundation
 
-final class LocalCartSaver: CartSaver {
+final class LocalItemSaver: CartSaver {
     
     func save(item: Item, completion: @escaping (CartSaver.Result) -> Void) {
         
