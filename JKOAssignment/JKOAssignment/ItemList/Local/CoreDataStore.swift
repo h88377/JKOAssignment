8f8c30d1b82d1507e1be924926ad5e59eb4c5626
Wayne <h88377@gmail.com>
2023-06-01 17:23:58 +0800

    Create `CoreDataStore` which conforms `ItemStoreSaver`

---
+++ JKOAssignment/JKOAssignment/ItemList/Local/CoreDataStore.swift
@@ -0,0 +1,14 @@
+//
+//  CoreDataStore.swift
+//  JKOAssignment
+//
+//  Created by 鄭昭韋 on 2023/6/1.
+//
+
+import Foundation
+
+final class CoreDataStore: ItemStoreSaver {
+    func insert(_ item: Item, completion: @escaping (ItemStoreSaver.Result) -> Void) {
+        
+    }
+}
