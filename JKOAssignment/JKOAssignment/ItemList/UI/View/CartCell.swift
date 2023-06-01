0b99fef9b99b82ee827fa2bb1bee23ec47764457
Wayne <h88377@gmail.com>
2023-06-01 21:15:35 +0800

    Move `CartCell` to its own file

---
+++ JKOAssignment/JKOAssignment/ItemList/UI/View/CartCell.swift
@@ -0,0 +1,78 @@
+//
+//  CartCell.swift
+//  JKOAssignment
+//
+//  Created by 鄭昭韋 on 2023/6/1.
+//
+
+import UIKit
+
+final class CartCell: UITableViewCell {
+    static let identifier = "\(CartCell.self)"
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let priceLabel: UILabel = {
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let itemImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let checkButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "square"), for: .normal)
+        button.setImage(UIImage(systemName: "checkmark.square"), for: .selected)
+        button.isSelected = true
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setUpUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpUI() {
+        contentView.addSubviews([checkButton, itemImageView, nameLabel, priceLabel])
+        
+        NSLayoutConstraint.activate([
+            checkButton.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            checkButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            
+            itemImageView.leadingAnchor.constraint(equalTo: checkButton.trailingAnchor, constant: 8),
+            itemImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            itemImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8),
+            itemImageView.widthAnchor.constraint(equalTo: itemImageView.heightAnchor),
+            itemImageView.heightAnchor.constraint(equalToConstant: 100),
+            
+            nameLabel.topAnchor.constraint(equalTo: itemImageView.topAnchor),
+            nameLabel.leadingAnchor.constraint(equalTo: itemImageView.trailingAnchor, constant: 8),
+            nameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            
+            priceLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
+            priceLabel.trailingAnchor.constraint(equalTo: nameLabel.trailingAnchor),
+            priceLabel.bottomAnchor.constraint(equalTo: itemImageView.bottomAnchor)
+        ])
+    }
+}
