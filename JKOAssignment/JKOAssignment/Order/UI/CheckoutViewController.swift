e2fd0880c914eb6c9d94f895b3a38e96bbf0b49c
Wayne <h88377@gmail.com>
2023-06-02 13:43:12 +0800

    Configure `CheckoutViewController` UI layout

--- JKOAssignment/JKOAssignment/Order/UI/CheckoutViewController.swift
+++ JKOAssignment/JKOAssignment/Order/UI/CheckoutViewController.swift
@@ -8,8 +8,38 @@
 import UIKit
 
 final class CheckoutViewController: UIViewController {
+    
+    // MARK: - Property
+    
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    private(set) lazy var checkoutButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemGray6
+        button.setTitle("提交訂單", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let priceLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .right
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     private let viewModel: CheckoutViewModel
     private let cellViewModels: [CheckoutCellViewModel]
+    
+    // MARK: - Life cycle
     
     init(viewModel: CheckoutViewModel, cellViewModels: [CheckoutCellViewModel]) {
         self.viewModel = viewModel
@@ -21,5 +51,30 @@ final class CheckoutViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUpUI()
+    }
     
+    // MARK: - Method
+    
+    private func setUpUI() {
+        view.backgroundColor = .white
+        view.addSubviews([tableView, priceLabel, checkoutButton])
+    
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            
+            priceLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            priceLabel.topAnchor.constraint(equalTo: tableView.bottomAnchor, constant: 16),
+            
+            checkoutButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            checkoutButton.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            checkoutButton.topAnchor.constraint(equalTo: priceLabel.bottomAnchor, constant: 16),
+            checkoutButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
 }
