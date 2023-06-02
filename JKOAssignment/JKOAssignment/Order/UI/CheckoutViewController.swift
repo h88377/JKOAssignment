7dbd4c713816499b0cfe71157e10163463731cdc
Wayne <h88377@gmail.com>
2023-06-02 13:48:39 +0800

    Create `CheckoutCell` with layout

--- JKOAssignment/JKOAssignment/Order/UI/CheckoutViewController.swift
+++ JKOAssignment/JKOAssignment/Order/UI/CheckoutViewController.swift
@@ -7,6 +7,65 @@
 
 import UIKit
 
+final class CheckoutCell: UITableViewCell {
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let priceLabel: UILabel = {
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let itemImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setUpUI()
+        selectionStyle = .none
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpUI() {
+        contentView.addSubviews([itemImageView, nameLabel, priceLabel])
+        
+        let imageHightConstraint = itemImageView.heightAnchor.constraint(equalToConstant: 100)
+        imageHightConstraint.priority = UILayoutPriority(999)
+        
+        NSLayoutConstraint.activate([
+            itemImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            itemImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            itemImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8),
+            itemImageView.widthAnchor.constraint(equalTo: itemImageView.heightAnchor),
+            imageHightConstraint,
+            
+            nameLabel.topAnchor.constraint(equalTo: itemImageView.topAnchor),
+            nameLabel.leadingAnchor.constraint(equalTo: itemImageView.trailingAnchor, constant: 8),
+            nameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            
+            priceLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
+            priceLabel.trailingAnchor.constraint(equalTo: nameLabel.trailingAnchor),
+            priceLabel.bottomAnchor.constraint(equalTo: itemImageView.bottomAnchor)
+        ])
+    }
+}
+
 final class CheckoutViewController: UIViewController {
     
     // MARK: - Property
