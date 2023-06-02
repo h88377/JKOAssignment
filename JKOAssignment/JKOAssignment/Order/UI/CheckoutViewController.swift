17179d539b804397b493ec887cb1e2d0f51f4fef
Wayne <h88377@gmail.com>
2023-06-02 13:23:02 +0800

    Create `CheckoutViewController` with `CheckoutViewModel` dependency

---
+++ JKOAssignment/JKOAssignment/Order/UI/CheckoutViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CheckoutViewController.swift
+//  JKOAssignment
+//
+//  Created by 鄭昭韋 on 2023/6/2.
+//
+
+import UIKit
+
+final class CheckoutViewController: UIViewController {
+    private let viewModel: CheckoutViewModel
+    
+    init(viewModel: CheckoutViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
