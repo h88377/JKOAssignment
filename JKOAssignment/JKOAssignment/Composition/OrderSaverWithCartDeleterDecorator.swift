9189d6b3488bd4dd1140e97d40c7b1beca08b12e
Wayne <h88377@gmail.com>
2023-06-05 15:21:05 +0800

    Create `OrderSaverWithCartDeleterDecorator` to add feature for `OrderSaver` and avoid `Order` layer couple with `ItemList` layer

---
+++ JKOAssignment/JKOAssignment/Composition/OrderSaverWithCartDeleterDecorator.swift
@@ -0,0 +1,36 @@
+//
+//  OrderSaverWithCartDeleterDecorator.swift
+//  JKOAssignment
+//
+//  Created by 鄭昭韋 on 2023/6/5.
+//
+
+import Foundation
+
+final class OrderSaverWithCartDeleterDecorator: OrderSaver {
+    private let decoratee: OrderSaver
+    private let cartDeleter: CartItemStoreDeleter
+    
+    init(decoratee: OrderSaver, cartDeleter: CartItemStoreDeleter) {
+        self.decoratee = decoratee
+        self.cartDeleter = cartDeleter
+    }
+    
+    func save(order: Order, completion: @escaping (OrderSaver.Result) -> Void) {
+        decoratee.save(order: order) { [weak self] error in
+            if let error = error {
+                completion(.some(error))
+            } else {
+                let localItems = order.items.map { $0.toLocal() }
+                self?.cartDeleter.delete(items: localItems, completion: completion)
+            }
+            
+        }
+    }
+}
+
+private extension Item {
+    func toLocal() -> LocalItem {
+        return LocalItem(name: name, description: description, price: price, timestamp: timestamp, imageName: imageName)
+    }
+}
