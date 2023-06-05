df94a5e0c00168d2c2252cdc54641bcdc7c6332f
Wayne <h88377@gmail.com>
2023-06-05 17:38:27 +0800

    Create `OrderHistoryViewModel` with `OrderLoader` dependency to handle `loadOrders`

---
+++ JKOAssignment/JKOAssignment/Order/UI/ViewModel/OrderHistoryViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  OrderHistoryViewModel.swift
+//  JKOAssignment
+//
+//  Created by 鄭昭韋 on 2023/6/5.
+//
+
+import Foundation
+
+final class OrderHistoryViewModel {
+    var isOrdersRefreshLoadingStateOnChanged: Observable<Bool>?
+    var isOrdersRefreshingStateOnChanged: Observable<[Order]>?
+    var isEmptyOrderStateOnChanged: Observable<String>?
+    var isOrdersRefreshingErrorStateOnChange: Observable<String>?
+    
+    private let orderLoader: OrderLoader
+    
+    init(orderLoader: OrderLoader) {
+        self.orderLoader = orderLoader
+    }
+    
+    func loadOrders() {
+        isOrdersRefreshLoadingStateOnChanged?(true)
+        orderLoader.loadOrders { [weak self] result in
+            switch result {
+            case let .success(orders) where orders.isEmpty:
+                self?.isEmptyOrderStateOnChanged?(OrderReminderMessage.noOrder.rawValue)
+                
+            case let .success(orders):
+                self?.isOrdersRefreshingStateOnChanged?(orders)
+                
+            case .failure:
+                self?.isOrdersRefreshingErrorStateOnChange?("")
+            }
+            self?.isOrdersRefreshLoadingStateOnChanged?(false)
+        }
+    }
+}
