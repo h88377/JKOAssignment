b032ce98a5298f1a8e79e69a66f350caf6e5905e
Wayne <h88377@gmail.com>
2023-06-05 17:00:53 +0800

    Create `LocalOrderLoader` to conform `OrderLoader` and hide framework detail with `OrderStoreLoader`

---
+++ JKOAssignment/JKOAssignment/Order/Local/LocalOrderLoader.swift
@@ -0,0 +1,64 @@
+//
+//  LocalOrderLoader.swift
+//  JKOAssignment
+//
+//  Created by 鄭昭韋 on 2023/6/5.
+//
+
+import Foundation
+
+struct LocalOrder {
+    let items: [LocalOrderItem]
+    let price: Int
+    let timestamp: Date
+}
+
+struct LocalOrderItem {
+    let name: String
+    let description: String
+    let price: Int
+    let timestamp: Date
+    let imageName: String
+}
+
+protocol OrderStoreLoader {
+    typealias Result = Swift.Result<[LocalOrder], Error>
+    
+    func retrieve(completion: @escaping (Result) -> Void)
+}
+
+final class LocalOrderLoader: OrderLoader {
+    enum Error: Swift.Error {
+        case failed
+    }
+    
+    private let storeLoader: OrderStoreLoader
+    
+    init(storeLoader: OrderStoreLoader) {
+        self.storeLoader = storeLoader
+    }
+    
+    func loadOrders(completion: @escaping (OrderLoader.Result) -> Void) {
+        storeLoader.retrieve { result in
+            switch result {
+            case let .success(localOrders):
+                completion(.success(localOrders.toModel()))
+                
+            case .failure:
+                completion(.failure(Error.failed))
+            }
+        }
+    }
+}
+
+private extension Array where Element == LocalOrder {
+    func toModel() -> [Order] {
+        return map { Order(items: $0.items.toModel(), price: $0.price, timestamp: $0.timestamp) }
+    }
+}
+
+private extension Array where Element == LocalOrderItem {
+    func toModel() -> [Item] {
+        return map { Item(name: $0.name, description: $0.description, price: $0.price, timestamp: $0.timestamp, imageName: $0.imageName) }
+    }
+}
