f5984fcaf2f13844836ddeffdd4551944ec2ea2c
Wayne <h88377@gmail.com>
2023-06-05 22:33:09 +0800

    Move `OrderHistoryCell` to its own file

--- JKOAssignment/JKOAssignment/Order/UI/Controller/OrderHistoryViewController.swift
+++ JKOAssignment/JKOAssignment/Order/UI/Controller/OrderHistoryViewController.swift
@@ -7,59 +7,6 @@
 
 import UIKit
 
-final class OrderHistoryCell: UITableViewCell {
-    let priceLabel: UILabel = {
-        let label = UILabel()
-        
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    let verticalStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.alignment = .fill
-        stackView.axis = .vertical
-        stackView.spacing = 8
-        stackView.distribution = .fillEqually
-        
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        return stackView
-    }()
-    
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        setUpUI()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func setUpUI() {
-        contentView.addSubviews([priceLabel, verticalStackView])
-        
-        NSLayoutConstraint.activate([
-            verticalStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
-            verticalStackView.trailingAnchor.constraint(equalTo: priceLabel.leadingAnchor, constant: 8),
-            verticalStackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
-            verticalStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8),
-            
-            priceLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
-            priceLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8)
-        ])
-    }
-    
-    func configureItems(with horizontalStackViews: [UIStackView]) {
-        verticalStackView.addArrangedSubviews(with: horizontalStackViews)
-    }
-}
-
-private extension UIStackView {
-    func addArrangedSubviews(with views: [UIView]) {
-        views.forEach(addArrangedSubview)
-    }
-}
-
 final class OrderHistoryViewController: UITableViewController {
     let noOrdersReminder: UILabel = {
         let label = UILabel()
