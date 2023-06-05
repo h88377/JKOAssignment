db3c4968ebe55107e93761457e4d82227065614f
Wayne <h88377@gmail.com>
2023-06-05 17:37:52 +0800

    Add `noOrder` reminder and `orderHistory` error message in constant enum

--- JKOAssignment/JKOAssignment/Order/Helpers/OrderConstants.swift
+++ JKOAssignment/JKOAssignment/Order/Helpers/OrderConstants.swift
@@ -9,8 +9,13 @@ import Foundation
 
 enum OrderErrorMessage: String {
     case checkout = "結帳失敗"
+    case orderHistory = "加載訂單記錄失敗"
 }
 
 enum OrderSuccessMessage: String {
     case checkout = "結帳成功"
+}
+
+enum OrderReminderMessage: String {
+    case noOrder = "目前無訂單記錄喔"
 }
