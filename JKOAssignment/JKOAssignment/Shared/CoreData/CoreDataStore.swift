3df573311d1248f39b10e6fb03701bbb8247c5f1
Wayne <h88377@gmail.com>
2023-06-06 23:44:51 +0800

    Move `CoreData` helper to its own file and group

--- JKOAssignment/JKOAssignment/Shared/CoreData/CoreDataStore.swift
+++ JKOAssignment/JKOAssignment/Shared/CoreData/CoreDataStore.swift
@@ -151,31 +151,3 @@ extension CoreDataStore: OrderStoreLoader {
         }
     }
 }
-
-// MARK: - Helper
-
-extension NSPersistentContainer {
-    enum LoadingError: Error {
-        case modelNotFound
-        case failedToLoadPersistentStore(Error)
-    }
-    
-    static func load(modelName: String, in bundle: Bundle, storeURL: URL) throws -> NSPersistentContainer {
-        guard let modelURL = bundle.url(forResource: modelName, withExtension: "momd"), let model = NSManagedObjectModel(contentsOf: modelURL) else {
-            throw LoadingError.modelNotFound
-        }
-        
-        let container = NSPersistentContainer(name: modelName, managedObjectModel: model)
-        let description = NSPersistentStoreDescription(url: storeURL)
-        container.persistentStoreDescriptions = [description]
-        
-        var loadError: Error?
-        container.loadPersistentStores { _, error in
-            loadError = error
-        }
-        
-        try loadError.map { throw LoadingError.failedToLoadPersistentStore($0) }
-        
-        return container
-    }
-}
