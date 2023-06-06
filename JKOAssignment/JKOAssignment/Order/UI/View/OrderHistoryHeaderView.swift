abf4dbb72fd46251a1542e5ecc8a9bbd97304b06
Wayne <h88377@gmail.com>
2023-06-06 19:07:12 +0800

    Reconfigure layout for `OrderHistoryHeaderView`

--- JKOAssignment/JKOAssignment/Order/UI/View/OrderHistoryHeaderView.swift
+++ JKOAssignment/JKOAssignment/Order/UI/View/OrderHistoryHeaderView.swift
@@ -38,13 +38,13 @@ final class OrderHistoryHeaderView: UITableViewHeaderFooterView {
         addSubviews([timestampLabel, priceLabel])
         
         NSLayoutConstraint.activate([
-            timestampLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
-            timestampLabel.topAnchor.constraint(equalTo: topAnchor, constant: 8),
-            timestampLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8),
+            priceLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            priceLabel.topAnchor.constraint(equalTo: topAnchor, constant: 8),
+            priceLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8),
             
-            priceLabel.topAnchor.constraint(equalTo: timestampLabel.topAnchor),
-            priceLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
-            priceLabel.bottomAnchor.constraint(equalTo: timestampLabel.bottomAnchor)
+            timestampLabel.topAnchor.constraint(equalTo: priceLabel.topAnchor),
+            timestampLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            timestampLabel.bottomAnchor.constraint(equalTo: priceLabel.bottomAnchor)
         ])
     }
 }
