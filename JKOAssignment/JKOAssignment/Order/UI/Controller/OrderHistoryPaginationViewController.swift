de9d56c8974a9885730554de5ecb29afbd4b5881
Wayne <h88377@gmail.com>
2023-06-06 21:31:22 +0800

    Create `OrderHistoryPaginationViewController` to handle pagination

---
+++ JKOAssignment/JKOAssignment/Order/UI/Controller/OrderHistoryPaginationViewController.swift
@@ -0,0 +1,41 @@
+//
+//  OrderHistoryPaginationViewController.swift
+//  JKOAssignment
+//
+//  Created by 鄭昭韋 on 2023/6/6.
+//
+
+import UIKit
+
+final class OrderHistoryPaginationViewController {
+    private(set) var isPaginating = false
+    
+    private let viewModel: OrderHistoryPaginationViewModel
+    
+    init(viewModel: OrderHistoryPaginationViewModel) {
+        self.viewModel = viewModel
+        self.setUpBinding()
+    }
+    
+    func resetPage() {
+        viewModel.resetPage()
+    }
+     
+    func paginate(on scrollView: UIScrollView) {
+        guard !isPaginating else { return }
+        
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+        let frameHeight = scrollView.frame.height
+        
+        if offsetY > (contentHeight - frameHeight) {
+            viewModel.loadNextPage()
+        }
+    }
+    
+    private func setUpBinding() {
+        viewModel.isOrdersPaginationLoadingStateOnChange = { [weak self] isPaginating in
+            self?.isPaginating = isPaginating
+        }
+    }
+}
