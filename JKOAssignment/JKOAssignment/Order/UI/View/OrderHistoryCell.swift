c005d54deabbd5ff3d8b7789bf984a0d4479ea14
Wayne <h88377@gmail.com>
2023-06-06 12:03:19 +0800

    Replace `OrderHistoryCell.configureItems` parameter to be array of `UIView`

--- JKOAssignment/JKOAssignment/Order/UI/View/OrderHistoryCell.swift
+++ JKOAssignment/JKOAssignment/Order/UI/View/OrderHistoryCell.swift
@@ -49,8 +49,8 @@ final class OrderHistoryCell: UITableViewCell {
         ])
     }
     
-    func configureItems(with horizontalStackViews: [UIStackView]) {
-        verticalStackView.addArrangedSubviews(with: horizontalStackViews)
+    func configureItems(with views: [UIView]) {
+        verticalStackView.addArrangedSubviews(with: views)
     }
 }
 
